5f7001839601c4eb98b5834ee1e6f531b67f44c1
Foodword <saahirba07@gmail.com>
2025-12-08 10:42:39 -0800

    Create List.cpp

---
+++ List.cpp
@@ -0,0 +1,22 @@
+#include "List.h"
+#include <iostream>
+
+void List::addItem(const Item &item) {
+    items.push_back(item);
+}
+
+void List::display() const {
+    std::cout << "\n===== TO DO LIST =====\n";
+
+    if (items.empty()) {
+        std::cout << "No items.\n";
+        return;
+    }
+
+    for (size_t i = 0; i < items.size(); i++) {
+        std::cout << i + 1 << ". "
+                  << items[i].getTitle()
+                  << " - Due: " << items[i].getDueDate()
+                  << "\n";
+    }
+}
